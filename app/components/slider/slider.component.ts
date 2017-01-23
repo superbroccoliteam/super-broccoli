7fd4110482c238dd00ba9226ee14a2c86c562df5
Thibault Seynaeve <thibault.seynaeve@telenet.be>
2017-01-23 22:35:39 +0100

    nieuwste versie

--- app/components/slider/slider.component.ts
+++ app/components/slider/slider.component.ts
@@ -7,21 +7,21 @@ import {Component} from '@angular/core'
     <div class="slideshow-container">
 
             <div class="mySlides fading">
-                <div class="numbertext">1 / 3</div>
-                <img src="http://gamewires.com/Images/Posts/48007_banner.jpg" style="width:100%">
-                <div class="text">Fifa 17</div>
+             
+                <img src="app/assets/img/ESL.jpg" style="width:100%">
+              
             </div>
 
             <div class="mySlides fading">
-                <div class="numbertext">2 / 3</div>
-                <img src="http://www.psnation.com/wp-content/uploads/2016/05/call-of-duty-infinite-warfare-banner.png" style="width:100%">
-                <div class="text">Infinite Warfare</div>
+         
+                <img src="app/assets/img/LCS.jpg" style="width:100%">
+            
             </div>
 
             <div class="mySlides fading">
-                <div class="numbertext">3 / 3</div>
-                <img src="http://telkomgaming.co.za/wp-content/uploads/2016/05/Overwatch-Banner-1-800x277.png" style="width:100%">
-                <div class="text">Overwatch</div>
+    
+                <img src="app/assets/img/MLG.jpg" style="width:100%">
+               
             </div>
 
             <a class="prev" onclick="plusSlides(-1)">❮</a>
